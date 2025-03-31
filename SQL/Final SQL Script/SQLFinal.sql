af5da2ce1b51b248bc7b3202fbb2bf937a46a915
KrastevIliya <Iliya.krastev@bs-erlangen.de>
2025-03-31 11:56:25 +0200

    spGetUnratedContestImages.txt

--- SQL/Final SQL Script/SQLFinal.sql
+++ SQL/Final SQL Script/SQLFinal.sql
@@ -100,6 +100,25 @@ CREATE TABLE tblMenu_Product (
 
 -- Procedures 
 
+-- spGetUnratedContestImages
+
+DELIMITER //
+
+create PROCEDURE spGetUnratedContestImages(IN UserKey int)
+BEGIN 
+
+select c.nKey as nKey,
+	   c.szImagePath,
+       l.szAccountName,
+       c.dtCreated
+from tblcontestimage c 
+join tbllogin l on l.nKey=c.nLoginKey
+where l.nKey = UserKey and c.bCanBeRated <> 0 ;
+
+END //
+
+DELIMITER ;
+
 -- spUpdateContestImageConfirmation
 
 DELIMITER //
