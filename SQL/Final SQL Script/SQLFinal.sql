8f6a3cd03b89de8ab7768251835c006408e11d9d
KrastevIliya <Iliya.krastev@bs-erlangen.de>
2025-03-31 12:31:51 +0200

    spUpdateContestImageRating

--- SQL/Final SQL Script/SQLFinal.sql
+++ SQL/Final SQL Script/SQLFinal.sql
@@ -100,6 +100,24 @@ CREATE TABLE tblMenu_Product (
 
 -- Procedures 
 
+-- spUpdateContestImageRating
+
+DELIMITER //
+
+CREATE PROCEDURE spUpdateContestImageRating(IN nUserKey INT, IN nImageKey INT, IN nMyRating INT)
+BEGIN
+  IF EXISTS (SELECT 1 FROM tblcontestratings c WHERE c.nLoginKey = nUserKey AND c.nContestImageKey = nImageKey) THEN
+    UPDATE tblcontestratings
+    SET nRating = nMyRating
+    WHERE nLoginKey = nUserKey AND nContestImageKey = nImageKey;
+  ELSE
+    INSERT INTO tblcontestratings (nContestImageKey, nLoginKey, nRating)
+    VALUES (nImageKey, nUserKey, nMyRating);
+  END IF;
+END //
+
+DELIMITER ;
+
 -- spGetUnratedContestImages
 
 DELIMITER //
