9adc2306de2b21efdafe21e65f5def0ed2324e4f
KrastevIliya <Iliya.krastev@bs-erlangen.de>
2025-04-01 09:09:43 +0200

    spInvoice

--- SQL/Final SQL Script/SQLFinal.sql
+++ SQL/Final SQL Script/SQLFinal.sql
@@ -99,6 +99,51 @@ CREATE TABLE tblMenu_Product (
 
 -- Procedures 
 
+-- spInvoice
+
+DELIMITER // 
+
+CREATE PROCEDURE spInvoice(IN nOrderKey int)
+
+BEGIN 
+
+-- Datetime
+
+select o.dtTime 
+from tblorder o 
+where o.nKey = nOrderKey; 
+
+-- Customer 
+
+SELECT c.* from tblorder o 
+join tblcustomer c on c.nKey=o.nCustomerKey
+where o.nKey=nOrderKey;
+
+-- Order
+SELECT p.szName as Name,
+       p.dPrice as Price,
+	   op.nQuantity as Quantity,
+       op.dOldPrice as Price,
+       op.nQuantity*op.dOldPrice as Gesamt   
+       from tblorder o 
+join tblorder_product op on o.nKey=op.nOrderKey
+join tblproduct p on p.nKey=op.nProductKey
+where o.nKey=nOrderKey;
+
+-- Total price
+SELECT 
+       sum(op.dOldPrice* op.nQuantity) * 0.93 as Brutto,
+       sum(op.dOldPrice* op.nQuantity) * 0.07 as MwS,
+       sum(op.dOldPrice* op.nQuantity) as Netto
+       from tblorder o 
+join tblorder_product op on o.nKey=op.nOrderKey
+join tblproduct p on p.nKey=op.nProductKey
+where o.nKey=nOrderKey;
+
+END //
+
+DELIMITER ;
+
 -- spGetContestWinners
 DELIMITER // 
 
