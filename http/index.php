993ff21731e22ce4b92fadd0667aeff9e4bdf5cf
Tom Rudolph <tomr@imbus.de>
2025-04-02 10:39:06 +0200

    Changes 02.04.2025

--- http/index.php
+++ http/index.php
@@ -62,44 +62,63 @@ $user = getCurrentUser();
         
         <!--Button class switch function (color change)-->
         <script>
-            function switchClasses(activeContent, inactiveContent) {
-                let orderButton = document.getElementById("orderButton");
-                let contestButton = document.getElementById("contestButton");
+            function switchClasses(activeContent, inactiveContent, doNotReload) {
+            let orderButton = document.getElementById("orderButton");
+            let contestButton = document.getElementById("contestButton");
         
-                if (activeContent === 'order' || activeContent === 'order') {
-                    orderButton.className = "btn btn-success";
-                    contestButton.className = "btn btn-secondary";
+            if (activeContent === 'order') {
+                orderButton.className = "btn btn-success";
+                contestButton.className = "btn btn-secondary";
+
+                if(!doNotReload) {
                     loadProductCategory(<?php echo current($productCategories)->getKey();?>);
-                } else if (activeContent === 'Funny_Dinner_Contest' || activeContent === 'Funny_Dinner_Contest_Rating') {
-                    orderButton.className = "btn btn-secondary";
-                    contestButton.className = "btn btn-success";
-                    loadDinnerContest(false);
                 }
+            } else if (activeContent === 'Funny_Dinner_Contest' || activeContent === 'Funny_Dinner_Contest_Rating') {
+                orderButton.className = "btn btn-secondary";
+                contestButton.className = "btn btn-success";
+
+                // Reset the color of all buttons to default
+                const buttons = document.querySelectorAll('.category-button');
+                buttons.forEach(btn => btn.style.setProperty('color', '#000', 'important'));
+
+                loadDinnerContest(false);
             }
-        </script>   
+            }
+        </script>
 
         <!--Side bar-->
         <div class="d-flex">
-            <div class="bg-light border" style="width: 200px; height: 100vh;">
-            <ul class="nav flex-column">
-                <!-- TODO: Has to be loaded dynamic --> 
-                <!-- TODO: loadContent should be changed a little bit. It should be split into two functions I think one for loading something like funny dinner contest one for loading the left sidebar thingies --> 
-                <!--Beliebt Content-->
-                <li class="nav-item">
-                </li>
-
-                <?php foreach($productCategories as $productCategory): ?>
-                    <li class="nav-item">
-                        <button class="btn btn-link nav-link" onclick="loadProductCategory(<?php echo $productCategory->getKey();?>)"><?php echo htmlspecialchars($productCategory->getName()); ?></button>
-                    </li>
-                <?php endforeach; ?>
+            <div class="bg-light border" style="width: 250px; height: 100vh;">
+            <ul class="nav flex-column p-3" style="font-size: 16px;">
+            <li class="nav-item mb-3">
+            <h5 class="text-center" style="color: #28a745;">Kategorien</h5>
+            </li>
+            <?php foreach($productCategories as $productCategory): ?>
+            <li onclick="switchClasses('order', 'Funny_Dinner_Contest', true)" class="nav-item mb-2">
+            <button class="btn btn-link nav-link text-start text-dark category-button" style="font-weight: 500; text-decoration: none;" onclick="highlightCategory(this); loadProductCategory(<?php echo $productCategory->getKey();?>)">
+            <?php echo htmlspecialchars($productCategory->getName()); ?>
+            </button>
+            </li>
+            <?php endforeach; ?>
             </ul>
             </div>
-            <div class="flex-grow-1 p-3" id="main-content">
+            <div id="main-content" style="flex-grow: 1; padding: 20px;">
             <!--Main content which will be displayed if nothing is selected-->
-            <h1>Main content which will be displayed if nothing is selected</h1>
+            <h1 class="text-center text-secondary">Willkommen! Wählen Sie eine Kategorie aus.</h1>
             </div>
         </div>
+
+        <script>
+            function highlightCategory(button) {
+            // Reset all buttons to default color
+            const buttons = document.querySelectorAll('.category-button');
+            buttons.forEach(btn => btn.style.setProperty('color', '#000', 'important'));
+
+            // Set the clicked button color to #28a745
+            button.style.setProperty('color', '#28a745', 'important');
+            }
+        </script>
+
 
         <!--In the event that a tab within the side navigation bar is engaged, the en                    <button class="btn btn-link nav-link" onclick="loadContent('Beliebt')">Beliebt</button>
 tirety of the web page shall remain stationary, eschewing a full-page reload. Instead, only the content area shall undergo a dynamic update, thus preserving the user’s seamless browsing experience.-->
