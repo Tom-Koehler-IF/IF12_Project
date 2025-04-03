abddec89c753a44fa76bf10bf35aad40f890c0ad
Tom Koehler <kohlert390@gmail.com>
2025-04-03 20:32:16 +0200

    Added admin report sp

--- SQL/Final SQL Script/SQLFinal.sql
+++ SQL/Final SQL Script/SQLFinal.sql
@@ -383,6 +383,71 @@ END$$
 DELIMITER ;
 
 
+-- spGetAdminReport
+DELIMITER //
+
+create procedure spGetAdminReport(
+    IN dtFrom DATE,
+    IN dtTill DATE
+)
+begin
+set dtTill = DATE_ADD(dtTill, INTERVAL 1 DAY);
+
+-- Erstes resultset für generelle informationen
+select
+        COUNT(distinct O.nKey) as nOrderCount
+    ,   SUM(OP.dOldPrice * OP.nQuantity) as dTotalRevenue
+    ,   SUM(OP.dOldPrice * OP.nQuantity) / NULLIF(COUNT(distinct O.nKey), 0) as dAvgOrderPrice
+    ,   SUM(OP.nQuantity) as nItemsSold
+from tblorder O
+join tblorder_product OP on OP.nOrderKey = O.nKey
+where O.dtTime between dtFrom and dtTill;
+
+-- Zweites resultset für tägliche informationen
+select
+        DATE(O.dtTime) as dtDay
+    ,   COUNT(distinct O.nKey) as nOrderCount
+    ,   SUM(OP.dOldPrice * OP.nQuantity) as dTotalRevenue
+    ,   SUM(OP.dOldPrice * OP.nQuantity) / NULLIF(COUNT(distinct O.nKey), 0) as dAvgOrderPrice
+    ,   SUM(OP.nQuantity) as nItemsSold
+from tblorder O
+join tblorder_product OP on OP.nOrderKey = O.nKey
+where O.dtTime between dtFrom and dtTill
+group by DATE(O.dtTime);
+
+-- Drittes resultset für top 5 produkte
+select
+        P.szName as szName
+    ,   SUM(OP.nQuantity) as nTotalQuantity
+    ,   SUM(OP.nQuantity * OP.dOldPrice) as dTotalRevenue
+    ,   (SUM(OP.nQuantity * OP.dOldPrice) / SUM(SUM(OP.nQuantity * OP.dOldPrice)) OVER ()) * 100 as dTotalPercentage
+from tblorder O
+join tblorder_product OP on OP.nOrderKey = O.nKey
+join tblproduct P on P.nKey = OP.nProductKey
+where O.dtTime between dtFrom and dtTill
+group by P.nKey, P.szName
+order by dTotalRevenue desc
+limit 5;
+
+-- Viertes resultset für top 5 kategorien
+select
+        P.szName as szName
+    ,   SUM(OP.nQuantity) as nTotalQuantity
+    ,   SUM(OP.nQuantity * OP.dOldPrice) as dTotalRevenue
+    ,   (SUM(OP.nQuantity * OP.dOldPrice) / SUM(SUM(OP.nQuantity * OP.dOldPrice)) OVER ()) * 100 as dTotalPercentage
+from tblorder O
+join tblorder_product OP on OP.nOrderKey = O.nKey
+join tblproduct P on P.nKey = OP.nProductKey
+join tblproduct_category C on C.nKey = P.nProduct_CategoryKey
+where O.dtTime between dtFrom and dtTill
+group by C.nKey, C.szName
+order by dTotalRevenue desc
+limit 5;
+
+end
+//
+
+DELIMITER ;
 
 
 -- Triggers
