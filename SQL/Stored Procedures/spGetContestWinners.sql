c90b98d71d14dbf41e9427f58a07c53048d84592
KrastevIliya <Iliya.krastev@bs-erlangen.de>
2025-04-03 21:46:12 +0200

    spGetContestWinners

--- SQL/Stored Procedures/spGetContestWinners.sql
+++ SQL/Stored Procedures/spGetContestWinners.sql
@@ -3,22 +3,37 @@ DELIMITER //
 CREATE PROCEDURE spGetContestWinners()
 
 BEGIN
-select *
-from (
-	SELECT 
+create TEMPORARY table tblwinners(nKey int,szImagePath varchar(50),szAccountName varchar(50), dtCreated datetime, FinalRating int );
+
+insert into tblwinners SELECT 
     	r.nKey,              
     	r.szImagePath,
     	l.szAccountName,
-    	r.dtCreated,
-    	SUM(c.nRating) AS FinalRating,
-    	ROW_NUMBER() OVER (PARTITION BY DATE_FORMAT(r.dtCreated, '%Y-%m') ORDER BY FinalRating desc) AS row_num
-	FROM tblContestRatings c
-	JOIN tblContestImage r ON r.nKey = c.nContestImageKey
-	JOIN tblLogin l ON l.nKey = r.nLoginKey
-    group by r.nKey
-) as test
-where row_num = 1;
-
+        r.dtCreated,
+    	SUM(c.nRating) AS FinalRating
+	FROM tblcontestratings c
+	JOIN tblcontestimage r ON r.nKey = c.nContestImageKey
+	JOIN tbllogin l ON l.nKey = r.nLoginKey
+    group by r.nKey, YEAR(r.dtCreated), MONTH(r.dtCreated);
+    
+select nKey,
+    szImagePath,
+    szAccountName,
+    dtCreated,
+    YEAR(dtCreated) as year,
+    MONTH(dtCreated) as month,
+    FinalRating
+FROM 
+    tblwinners w
+WHERE 
+    FinalRating = (
+        SELECT MAX(FinalRating)
+        FROM tblwinners w2
+        WHERE YEAR(w2.dtCreated) = year(w.dtCreated)
+          AND MONTH(w2.dtCreated) = month(w.dtCreated)
+    )
+ORDER BY 
+    dtCreated;
 END //
 
 DELIMITER ;
